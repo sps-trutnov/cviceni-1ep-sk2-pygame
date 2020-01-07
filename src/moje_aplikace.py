d46bffc588b6457bf1ec7e29c8b0c27e8a6c4293
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-01-07 08:20:16 +0100

    Reakce na uživatelské vstupy z klávesnice

--- src/moje_aplikace.py
+++ src/moje_aplikace.py
@@ -5,38 +5,60 @@ pygame.init()
 
 okno = pygame.display.set_mode((640, 480))
 
-x = 400
-y = 100
+el_x = 400
+el_y = 300
+
+ob_y = 100
+ob_v = 0.1
+ob_blue = 0
+
 w = 120
 
 v = 0.05
 
+# nekonecna smycka generujici framy obrazu
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             sys.exit()
     
-    x = x + 0.01
-    y = y - v
+    # ziskam informace o stisknutych klavesach
+    stisknuto = pygame.key.get_pressed()
     
-    # aby mi obdelnik neutikal nahoru
-    if y < 0:
-        y = 0
-        v = v * -1
-    # aby mi obdelnik neutikal vubec
-    if y > 480 - 75:
-        y = 480 - 75
-        v = v * -1
+    # vypnuti aplikace klavesou ESC
+    if stisknuto[pygame.K_ESCAPE]:
+        sys.exit()
     
+    # pohnu obdelnikem pomoci sipek
+    if stisknuto[pygame.K_UP]:
+        ob_y = ob_y - ob_v
+    if stisknuto[pygame.K_DOWN]:
+        ob_y = ob_y + ob_v
+    if stisknuto[pygame.K_b]:
+        ob_blue += 0.1
+    
+    # aby mi obdelnik neutikal z okna
+    if ob_y < 0:
+        ob_y = 0
+    if ob_y > 480 - 75:
+        ob_y = 480 - 75
+    
+    # aby mi obdelnik prilis nezmodral
+    if ob_blue > 255:
+        ob_blue = 255
+    
+    # automaticky pohyb elipsy
+    el_x = el_x + 0.01
+
     # aby mi elipsa neutikala doprava
-    if x > 640 - w:
-        x = 640 - w
+    if el_x > 640 - w:
+        el_x = 640 - w
     
     # premazani okna barvou pozadi
     okno.fill((255, 255, 255))
     # vykresleni obdelniku
-    pygame.draw.rect(okno, (0, 0, 0), (200, y, 150, 75))
+    pygame.draw.rect(okno, (0, 0, ob_blue), (200, ob_y, 150, 75))
     # vykresleni elipsy
-    pygame.draw.ellipse(okno, (0, 0, 255), (x, 300, w, 80))
+    pygame.draw.ellipse(okno, (0, 0, 255), (el_x, el_y, w, 80))
     
     pygame.display.update()
